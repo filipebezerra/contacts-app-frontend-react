8f6dae1e8df8160affeba3573856ae3dfe6613b5
Filipe Bezerra <filipebzerra@gmail.com>
2019-04-05 02:52:25 -0300

    Fixed removing unecessary starter code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
+
 
 class App extends Component {
   render() {
-    const contacts = [
-      { name: "Juliette" },
-      { name: "John" },
-      { name: "Phillip" },
-      { name: "Marie" }
-    ];
     return (
       <div className="App">
         Hello World
