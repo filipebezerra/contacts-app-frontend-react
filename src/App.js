b39d5a324e488b3a785554492b29a6f661583cd2
Filipe Bezerra <filipebzerra@gmail.com>
2019-04-20 16:58:15 -0300

    Removing a contact

--- src/App.js
+++ src/App.js
@@ -14,9 +14,11 @@ class App extends Component {
   }
 
   removeContact = (contact) => {
-    this.setState((state) => ({
-      contacts: state.contacts.filter((c) => c.id !== contact.id)
-    }))
+    ContactsAPI.remove(contact).then((contact) => {
+      this.setState((state) => ({
+        contacts: state.contacts.filter((c) => c.id !== contact.id)
+      }))
+    })
   }
 
   render() {
